e0d125aaf35c2da0da4cdcf07a584826eb3e7386
Shin Bamba <sbamba@stuy.edu>
2018-09-19 22:44:13 -0400

    08_lemme_flask_u_sumpn

---
+++ 08_lemme_flask_u_sumpn/08_lemme_flask_u_sumpn.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__) #instantiates the Flask class using a constructor
+
+@app.route('/route1') #route 1
+def nameOfThing():
+	return 'Please Work'
+
+@app.route('/route2') #route 2
+def anotherNameOfThing():
+	return 'This another route'
+	
+@app.route('/route3') #route 3
+def name3BecauseIHaveNoIdeasLeft():
+	return 'I do believe this is route 3'
+	
+app.debug = True
+app.run()
