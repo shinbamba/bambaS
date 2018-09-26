19a8f00ff8cddddef1f965b74d88b3b36717a795
Shin Bamba <sbamba@stuy.edu>
2018-09-25 22:31:19 -0400

    created /data and /util directories

--- 10_occupy_flask_st/template.py
+++ 10_occupy_flask_st/template.py
@@ -3,33 +3,10 @@
 #K10 -- Jinja Tuning
 #2018-09-22
 
+from util import occupations
 from flask import Flask ,  render_template
-import csv
-import random
 app = Flask(__name__) #instantiates the Flask class using a constructor
 
-def table(file_name):
-    values = {} #initialize dictionary
-    #opens the file for reading as a csv file
-    with open('occupations.csv') as csvfile:
-        #special type of reading that uses the first row as labels for the columns
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            #create key-value pairs for each row
-            values[row['Job Class']] = float(row['Percentage']) 
-        return values
-
-dictionary = table('occupations.csv') #dictionary created from csv file
-
-#returns a random occupation
-def pick_job(dictionary):
-     rand_float = random.uniform(0, 99.8) #generates a random float in range [0, 99.8]
-     if rand_float < 6.1: #when it is less than the first percentage
-         return "No job found :("
-     for occupation in dictionary:
-         rand_float -= dictionary[occupation] #subtract each percentage going down the list
-         if rand_float <= 0: #has arrived at the right interval
-             return occupation
 
 @app.route("/")
 def homepage():
@@ -39,10 +16,12 @@ def display_occ():
     return render_template("template.html",
                            title = "Occupations" ,
 			   head = " Table containing information about occupations in the United States (courtesy of Mr. Brooks) -Mr.Brown",
-                           randOccupation = pick_job(dictionary),
+                           randOccupation = occupations.pick_job(dictionary),
                            dict = dictionary)
 	
 
+dictionary = occupations.table('occupations.csv') #dictionary created from csv file	
+	
 if (__name__) == ("__main__"):
     app.debug = True
     app.run()
---
+++ 10_occupy_flask_st/util/occupations.py
@@ -0,0 +1,30 @@
+#Team Matcha Dog -- Shin Bamba, Joyce Liao
+#SoftDev1 pd8
+#K10 -- Jinja Tuning Refactored Code
+#2018-09-22
+
+import csv
+import random
+
+def table(file_name):
+    values = {} #initialize dictionary
+    #opens the file for reading as a csv file
+    with open('data/occupations.csv') as csvfile:
+        #special type of reading that uses the first row as labels for the columns
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #create key-value pairs for each row
+            values[row['Job Class']] = float(row['Percentage']) 
+        return values
+
+
+
+#returns a random occupation
+def pick_job(dictionary):
+     rand_float = random.uniform(0, 99.8) #generates a random float in range [0, 99.8]
+     if rand_float < 6.1: #when it is less than the first percentage
+         return "No job found :("
+     for occupation in dictionary:
+         rand_float -= dictionary[occupation] #subtract each percentage going down the list
+         if rand_float <= 0: #has arrived at the right interval
+             return occupation
