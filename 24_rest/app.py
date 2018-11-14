2fd0b608dc03cd75afd34f937dd31b49d01e4dd7
Shin Bamba <sbamba@stuy.edu>
2018-11-13 22:52:11 -0500

    wonder

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 import urllib
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def yes():
-    key = "https://api.nasa.gov/planetary/apod?api_key=gpIAYj6MqHViKv7ezcm7fLB7uFykIcONmM1SpOF3"
-    print(urllib.request.urlopen(key))
-    return render_template("index.html")
-
+	key = "https://api.nasa.gov/planetary/apod?date=2018-11-10&api_key=gpIAYj6MqHViKv7ezcm7fLB7uFykIcONmM1SpOF3"
+	url = urllib.request.urlopen(key)
+	r = url.read()
+	dict = json.loads(r)
+	print(json.loads(r))
+	print(urllib.request.urlopen(key))
+	return render_template("index.html", pic = dict['url'], exp = dict['explanation'])
 
 
 
