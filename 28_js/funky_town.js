cab72f678d3372ae46ff580cca77db2de56457a0
ShinBamba <sbamba@stuy.edu>
2018-12-19 13:24:03 -0500

    xd

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -1,7 +1,13 @@
+// Team Birdies - Shin Bamba, Johnson Li
+// SoftDev1 pd8
+// K28 -- Sequential Progression
+// 2018-12-19T
+
 var fibonacci = (n) => {
     return fibHelp(1, 0, n);
 };
 
+// Helper for fibonacci
 var fibHelp = (startNum, sumSoFar, numTimes) => {
     if (numTimes == 0){
         return sumSoFar;
